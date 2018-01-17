7b0b1ca890e60b052cba48f8c247c5b6e5fb09ea
Sebastian Bair <sebastian.bair@googlemail.com>
2018-01-17 13:09:25 +0100

    Most part of restructuring (contains errors in AStar and container classes)

--- src/main/java/de/hsnr/osm2018/server/controller/DataController.java
+++ src/main/java/de/hsnr/osm2018/server/controller/DataController.java
@@ -1,6 +1,7 @@
 package de.hsnr.osm2018.server.controller;
 
 import de.hsnr.osm2018.data.data.FilteredDataProvider;
+import org.json.JSONObject;
 import spark.Request;
 import spark.Response;
 
@@ -17,41 +18,52 @@ public class DataController extends JSONController {
         String[] path = request.pathInfo().substring(1).split("/");
         switch (path[1]) {
             case "point":
-                if (path.length < 4) {
-                    return error(response, "invalid route. usage: '/data/point/:latitude/:longitude/'");
-                }
-                double latitude;
-                try {
-                    latitude = Double.parseDouble(path[2]);
-                    if (latitude > 180D || latitude < -180D) {
-                        throw new NumberFormatException("value out of bound");
-                    }
-                } catch (NumberFormatException e) {
-                    return error(response, "invalid value for latitude");
-                }
-                double longitude;
-                try {
-                    longitude = Double.parseDouble(path[3]);
-                    if (longitude > 180D || longitude < -180D) {
-                        throw new NumberFormatException("value out of bound");
-                    }
-                } catch (NumberFormatException e) {
-                    return error(response, "invalid value for longitude");
-                }
-                double vicinityRadius = 0.01D; //TODO: adjust to a more realistic value
-                if (request.queryParams().contains("radius")) {
-                    try {
-                        vicinityRadius = Double.parseDouble(request.queryParams("radius"));
-                        if (vicinityRadius < 0D || vicinityRadius > 1D) {
-                            throw new NumberFormatException("value out of bound");
-                        }
-                    } catch (NumberFormatException e) {
-                        return error(response, "invalid value for radius");
-                    }
-                }
-                return success(response, "graph around the point", mProvider.getGraph(latitude, longitude, vicinityRadius).toJSON());
+                return handlePoint(request, response, path);
             default:
                 return error(response, "route not found");
         }
+    }
+
+    private JSONObject handlePoint(Request request, Response response, String[] path) {
+        if (path.length < 4) {
+            return error(response, "invalid route. usage: '/data/point/:latitude/:longitude/'");
+        }
+        double latitude;
+        try {
+            latitude = getCoordinateValue(path[2]);
+        } catch (NumberFormatException e) {
+            return error(response, "invalid value for latitude");
+        }
+        double longitude;
+        try {
+            longitude = getCoordinateValue(path[3]);
+        } catch (NumberFormatException e) {
+            return error(response, "invalid value for longitude");
+        }
+        double vicinityRadius = 0.01D; //TODO: adjust to a more realistic value
+        if (request.queryParams().contains("radius")) {
+            try {
+                vicinityRadius = getRadiusValue(request.queryParams("radius"), 1D);
+            } catch (NumberFormatException e) {
+                return error(response, "invalid value for radius");
+            }
+        }
+        return success(response, "graph around the point", mProvider.getGraph(latitude, longitude, vicinityRadius).toJSON());
+    }
+
+    private double getCoordinateValue(String requestValue) throws NumberFormatException {
+        double value = Double.parseDouble(requestValue);
+        if (value > 180D || value < -180D) {
+            throw new NumberFormatException("value out of bound");
+        }
+        return value;
+    }
+
+    private double getRadiusValue(String requestValue, double maxValue) throws NumberFormatException {
+        double value = Double.parseDouble(requestValue);
+        if (value < 0D || value > maxValue) {
+            throw new NumberFormatException("value out of bound");
+        }
+        return value;
     }
 }
