b9b23e81d3d2cf637699c5d0a6d782c132dcaa4a
Sebastian Bair <sebastian.bair@googlemail.com>
2018-01-20 19:35:21 +0100

    Implemented API-route for searching path from tow geo-coordinates to each other

--- src/main/java/de/hsnr/osm2018/server/controller/RouteController.java
+++ src/main/java/de/hsnr/osm2018/server/controller/RouteController.java
@@ -23,11 +23,12 @@ public class RouteController extends JSONController {
         switch (path[1]) {
             case "node":
                 return handleNode(request, response, path);
+            case "point":
+                return handlePoint(request, response, path);
             default:
                 return error(response, "route not found");
         }
     }
-
 
     private JSONObject handleNode(Request request, Response response, String[] path) {
         if (path.length < 5) {
@@ -73,11 +74,63 @@ public class RouteController extends JSONController {
         return success(response, "path data", data);
     }
 
+    private JSONObject handlePoint(Request request, Response response, String[] path) {
+        if (path.length < 7) {
+            return error(response, "invalid route. usage: '/data/point/:<shortest|fastest>/:startLatitude/:startLongitude/:destinationLatitude/:destinationLongitude/'");
+        }
+        boolean shortest;
+        if (path[2].equalsIgnoreCase("shortest")) {
+            shortest = true;
+        } else if (path[2].equalsIgnoreCase("fastest")) {
+            shortest = false;
+        } else {
+            return error(response, "Invalid value for type. allowed values are: shortest, fastest");
+        }
+        double startLatitude, startLongitude, destinationLatitude, destinationLongitude;
+        try {
+            startLatitude = getCoordinateValue(path[3]);
+            startLongitude = getCoordinateValue(path[4]);
+            destinationLatitude = getCoordinateValue(path[5]);
+            destinationLongitude = getCoordinateValue(path[6]);
+        } catch (NumberFormatException e) {
+            return error(response, "invalid value");
+        }
+        Node start = mProvider.getGraph().getNearest(startLatitude, startLongitude), destination = mProvider.getGraph().getNearest(destinationLatitude, destinationLongitude);
+        System.out.println("selected nodes: " + start + " - " + destination);
+        AStar algorithm = new AStar();
+        if (shortest) {
+            algorithm.runAStar(mProvider.getGraph(), start, destination);
+        } else {
+            algorithm.runAStarWithSpeed(mProvider.getGraph(), start, destination);
+        }
+        JSONArray data = new JSONArray();
+        int id = 0;
+        for (NodeContainer n : algorithm.getPath(start, destination)) {
+            id++;
+            JSONObject element = new JSONObject();
+            element.put("id", id);
+            element.put("lat", n.getLatitude());
+            element.put("lon", n.getLongitude());
+            element.put("node", n.getId());
+            element.put("w", 0);
+            data.put(element);
+        }
+        return success(response, "path data", data);
+    }
+
     private Node getNode(String requestValue) {
         Node node = mProvider.getGraph().getNode(Long.valueOf(requestValue));
         if (node == null) {
             throw new NumberFormatException("invalid node id");
         }
         return node;
+    }
+
+    private double getCoordinateValue(String requestValue) throws NumberFormatException {
+        double value = Double.parseDouble(requestValue);
+        if (value > 180D || value < -180D) {
+            throw new NumberFormatException("value out of bound");
+        }
+        return value;
     }
 }
